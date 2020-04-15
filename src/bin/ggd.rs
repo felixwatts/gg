32271ea0f8758a22628e50ddb9c46d0a676ce87e
Felix Watts <felix@provenance.org>
2020-04-15 14:42:53 +0100

    try to fix azure build

--- src/bin/ggd.rs
+++ src/bin/ggd.rs
@@ -1,31 +1,37 @@
 
 extern crate gg;
+#[cfg(target_os = "linux")]
 extern crate daemonize;
 
+#[cfg(target_os = "linux")]
 use std::fs::File;
+#[cfg(target_os = "linux")]
 use daemonize::Daemonize;
 use gg::err::GgResult;
 
+
 pub fn main() -> GgResult { 
+    #[cfg(target_os = "linux")]
+    {
+        let stdout = File::create("/tmp/ggd.out").unwrap();
+        let stderr = File::create("/tmp/ggd.err").unwrap();
 
-    let stdout = File::create("/tmp/ggd.out").unwrap();
-    let stderr = File::create("/tmp/ggd.err").unwrap();
+        let daemonize = Daemonize::new()
+            .pid_file("/tmp/ggd.pid")
+            .stdout(stdout)
+            .stderr(stderr)
+            .exit_action(|| println!("ggd started"));
 
-    let daemonize = Daemonize::new()
-        .pid_file("/tmp/ggd.pid")
-        .stdout(stdout)
-        .stderr(stderr)
-        .exit_action(|| println!("ggd started"));
-
-    match daemonize.start() {
-        Ok(_) => {
-            let mut setup = gg::setup::new_server()?;
-            loop{
-                setup.step().unwrap();
-            }
-        },
-        Err(e) => Err(e.into()),
+        match daemonize.start() {
+            Ok(_) => {
+                let mut setup = gg::setup::new_server()?;
+                loop{
+                    setup.step().unwrap();
+                }
+            },
+            Err(e) => Err(e.into()),
+        }
     }
 
-
+    Ok(())
 }
